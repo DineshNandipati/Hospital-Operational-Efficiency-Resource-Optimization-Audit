6a3acc948e58d3c0b31aa79b994ae5c20b00b3c7
DineshNandipati <dineshnandipati396@gmail.com>
2026-07-03 20:46:50 +0530

    DAY 5

---
+++ sql/DAY 5.sql
@@ -0,0 +1,34 @@
+-- DATE DIFFERENCE
+
+use hospital ;
+SELECT  *,
+    DATEDIFF(Discharge_Date, Admission_Date) AS date_difference
+FROM patient;
+
+-- SHORT , STANDARD , EXTENDED
+
+SELECT
+    *,
+    DATEDIFF(Discharge_Date, Admission_Date) AS date_difference,
+    CASE
+        WHEN DATEDIFF(Discharge_Date, Admission_Date) < 3 THEN 'Short'
+        WHEN DATEDIFF(Discharge_Date, Admission_Date) BETWEEN 3 AND 7 THEN 'Standard'
+        ELSE 'Extended'
+    END AS LOS_Cohort
+FROM patient;
+
+-- LOS 
+
+SELECT
+    Patient_ID,
+    Admission_Date,
+    Discharge_Date,
+    DATEDIFF(Discharge_Date, Admission_Date) AS LOS
+FROM patient
+WHERE DATEDIFF(Discharge_Date, Admission_Date) <= 0;
+
+--  invalid los (length of slay) 
+
+SELECT COUNT(*) AS Invalid_LOS
+FROM patient
+WHERE DATEDIFF(Discharge_Date, Admission_Date) <= 0;
